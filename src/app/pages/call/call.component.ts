d5970b5ac7f70355ed8a3fe33945af42e30ac8dd
Mario Uzae <mario.uzae@ontop.inf.br>
2018-08-01 18:27:37 -0300

    Call components icons and design. Fixed no audio webRTC

--- src/app/pages/call/call.component.ts
+++ src/app/pages/call/call.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import * as SIP from 'sip.js/dist/sip';
 
@@ -8,14 +8,23 @@ import * as SIP from 'sip.js/dist/sip';
   styleUrls: ['./call.component.css']
 })
 export class CallComponent implements OnInit {
-
+  
+  @ViewChild('remoteVideo') remoteVideo: ElementRef;
+  @ViewChild('localVideo') localVideo: ElementRef;
+  @ViewChild('remoteAudio') remoteAudio: ElementRef;
   user: { name: string };
+  imgUrl: string;
+  videoUrl: string;
+
   constructor(private router: ActivatedRoute, private route: Router) { }
 
   ngOnInit() {
     this.user = {
       name: this.router.snapshot.params['name']
     };
+
+    this.imgUrl = "assets/microphone.png";
+    this.videoUrl = "assets/video.png";
 
     var userAgent = new SIP.UA({
       uri: '199@18.211.195.231',
@@ -24,23 +33,58 @@ export class CallComponent implements OnInit {
       },
       authorizationUser: '199',
       password: '199',
-      register: true,
-      registrarServer: '18.211.195.231:8089'
-    });
-
-    var session = userAgent.invite('200@18.212.213.193', {
-      sessionDescriptionHandlerOptions: {
-        constraints: {
-          audio: true,
-          video: false
-        }
+      register: false,
+      registrarServer: 'sip:18.211.195.231',
+      turnServers: {
+        urls:"turn:numb.viagenie.ca",
+        username:"mariouzae@gmail.com",
+        password:"dasilva"
       }
     });
+
+    // var session = userAgent.invite('200@18.212.213.193', {
+    //   sessionDescriptionHandlerOptions: {
+    //     constraints: {
+    //       audio: true,
+    //       video: false
+    //     }
+    //   }
+    // });
+
+    // session.on('trackAdded', () => {
+    //   var pc = session.sessionDescriptionHandler.peerConnection;
+      
+    //   // Gets remote tracks
+    //   var remoteStream = new MediaStream();
+    //   pc.getReceivers().forEach(function(receiver) {
+    //     remoteStream.addTrack(receiver.track);
+    //   });
+    //   this.remoteVideo.nativeElement.srcObject = remoteStream;
+
+    //   // Gets local tracks
+    //   var localStream = new MediaStream();
+    //   pc.getSenders().forEach(function(sender) {
+    //     localStream.addTrack(sender.track);
+    //   });
+    //   this.localVideo.nativeElement.srcObject = localStream;
+    // });
+
+   
 
   }
 
   goBack() {
     this.route.navigate(['']);
+  }
+
+  mute()
+  {
+    this.imgUrl == "assets/microphone.png" ? this.imgUrl = "assets/cut-microphone.png" : this.imgUrl = "assets/microphone.png";
+  }
+
+  video()
+  {
+    this.videoUrl == "assets/video.png" ? this.videoUrl = "assets/cut-video.png" : this.videoUrl = "assets/video.png";
   }
 
 }
